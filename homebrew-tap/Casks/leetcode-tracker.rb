6abc1831a3352259d5c8bee4d6a2cfe9e24be92f
Rustam Karimov <kerimov.rustam@live.ru>
2026-04-11 14:49:20 +0200

    chore: bump version to 1.0.1

--- homebrew-tap/Casks/leetcode-tracker.rb
+++ homebrew-tap/Casks/leetcode-tracker.rb
@@ -1,9 +1,9 @@
 cask "leetcode-tracker" do
-  version "1.0.0"
+  version "1.0.1"
 
   if Hardware::CPU.arm?
     url "https://github.com/rustamPy/leetcode-tracker/releases/download/v#{version}/LeetCode%20Tracker-#{version}-arm64.dmg"
-    sha256 "3835cdadba6d6a118804c068a044d418070d230e8d310f4fda2cd69b818b2166"
+    sha256 "201ea7a7f8daa46adcd27376b6a7286a4e537ccbe26bf6d207226f8f8c392e23"
   else
     url "https://github.com/rustamPy/leetcode-tracker/releases/download/v#{version}/LeetCode%20Tracker-#{version}-x64.dmg"
     sha256 :no_check  # update with x64 SHA256 after first x64 build
