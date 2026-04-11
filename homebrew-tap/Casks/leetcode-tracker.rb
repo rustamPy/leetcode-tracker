37a1e52558ce78ebe96f1589b48209051b3b85cd
Rustam Karimov <kerimov.rustam@live.ru>
2026-04-11 14:53:18 +0200

    fix: correct cask URL (dots not spaces) and SHA256 from actual release asset

--- homebrew-tap/Casks/leetcode-tracker.rb
+++ homebrew-tap/Casks/leetcode-tracker.rb
@@ -2,10 +2,10 @@ cask "leetcode-tracker" do
   version "1.0.1"
 
   if Hardware::CPU.arm?
-    url "https://github.com/rustamPy/leetcode-tracker/releases/download/v#{version}/LeetCode%20Tracker-#{version}-arm64.dmg"
-    sha256 "201ea7a7f8daa46adcd27376b6a7286a4e537ccbe26bf6d207226f8f8c392e23"
+    url "https://github.com/rustamPy/leetcode-tracker/releases/download/v#{version}/LeetCode.Tracker-#{version}-arm64.dmg"
+    sha256 "efd13ccfb4b507dc3e73f04263288a463425f763f1edc08b2587bf5d1d19bb7d"
   else
-    url "https://github.com/rustamPy/leetcode-tracker/releases/download/v#{version}/LeetCode%20Tracker-#{version}-x64.dmg"
+    url "https://github.com/rustamPy/leetcode-tracker/releases/download/v#{version}/LeetCode.Tracker-#{version}-x64.dmg"
     sha256 :no_check  # update with x64 SHA256 after first x64 build
   end
 
